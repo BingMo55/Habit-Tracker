28933cfe0046dba1d2343a5a8d46f680191d9628
bing <bingmo55@gmail.com>
2020-02-01 12:45:58 -0800

    adding mongodb connection file

---
+++ connect.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+class MongoDB:
+    def __init__(self):
+        self.client = MongoClient("mongodb+srv://hackuci2020:<hack2020>@synitheia-nliiq.mongodb.net/test?retryWrites=true&w=majority")
+        self.db = self.
+
+
+    # def 
+def main():
+    connect = MongoDB()
+
+main()
