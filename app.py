dbe77686003e3b9b2ff0cc107195b04043507bcc
utsavjainb <utsavjainb@gmail.com>
2020-02-01 21:32:52 -0800

    added login

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def form():
 @app.route('/user', methods=['POST'])
 def user():
 	if request.method == 'POST':
-		print("request", request.form)
+		#print("request", request.form)
 		name = request.form["name"]
 		username = request.form["username"]
 		email = request.form["email"]
@@ -42,6 +42,32 @@ def user():
 		#db.add_user("JOHNNY", "uts@uci.edu", "sda", "dsadsa", "habit")	
 		db.add_user(name, email, password, number, habit)	
 		return jsonify({'args': request.form})
+
+@app.route('/login', methods=['POST'])
+def login():
+	if request.method == "POST":
+		print(request.form)
+		email = request.form["email"]
+		password = request.form["password"]
+		
+		rpass = db.get_val(email, "password")
+		if password == rpass:
+			return db.get_val(email, "habit")
+			#return jsonify(True)
+		else:
+			return jsonify(False)
+
+	
+
+
+@app.route('/getuserval', methods=['GET', 'POST'])
+def getuserval():
+	if request.method == 'POST':
+		print(request.form)
+		email = request.form["email"]
+		key = request.form["key"]
+		r = db.get_val(email, key)
+		return jsonify(r)
 
 
 if __name__ == '__main__':
